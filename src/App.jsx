315e794262d33659439187d6303cf08efca5e148
Tamanna@5092 <tamannaaktermoni51@gmail.com>
2026-03-05 13:07:21 +0600

    banner with img

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import Banner from "./components/Banner/Banner";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <div className="bg-[#faf6f6] py-10">
+        <div className="max-w-7xl mx-auto">
+          <Banner></Banner>
+        </div>
+      </div>
     </>
   );
 }
